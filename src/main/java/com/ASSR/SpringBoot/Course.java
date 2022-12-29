ce661f3a7b92d899b72fe4dfae3deee77364db1f
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2022-12-30 00:36:11 +0530

    logging is set to debug level

--- src/main/java/com/ASSR/SpringBoot/Course.java
+++ src/main/java/com/ASSR/SpringBoot/Course.java
@@ -1,5 +1,6 @@
 package com.ASSR.SpringBoot;
 
+
 public class Course {
 
 	//Constructor
--- src/main/java/com/ASSR/SpringBoot/CourseController.java
+++ src/main/java/com/ASSR/SpringBoot/CourseController.java
@@ -3,15 +3,16 @@ package com.ASSR.SpringBoot;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
 
-	
+	@RequestMapping("/courses")
 	public List<Course> course(){
 		return Arrays.asList(
-				new Course(1,"JAVA","Sajal Rastogi"),
+				new Course(1,"JAVA","Sajal Rastogi ASSR hello world"),
 				new Course(2,"MicroServices","Sajal Rastogi")
 				);
 	}
