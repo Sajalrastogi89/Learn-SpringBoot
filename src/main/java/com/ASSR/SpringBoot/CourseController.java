85b0741fd4f3a4f510c1ec46cc36a8b03b6dca63
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2023-01-02 22:50:42 +0530

    updated

--- src/main/java/com/ASSR/SpringBoot/CourseController.java
+++ src/main/java/com/ASSR/SpringBoot/CourseController.java
@@ -13,7 +13,7 @@ public class CourseController {
 	public List<Course> course(){
 		return Arrays.asList(
 				new Course(1,"JAVA","Sajal Rastogi"),
-				new Course(2,"MicroServices","Sajal Rastog"),
+				new Course(2,"MicroServices","Sajal Rastogi"),
 				new Course(3,"Spring Cloud","Sajal Rastogi"),
 				new Course(4,"Full Stack","Sajal Rastogi"),
 				new Course(5,"Spring Security","Sajal Rastogi"),
