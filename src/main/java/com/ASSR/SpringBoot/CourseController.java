abb58b292080e3c01a2808f2314904c383e09508
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2023-01-03 20:56:47 +0530

    Minor bug fixed

--- src/main/java/com/ASSR/SpringBoot/CourseController.java
+++ src/main/java/com/ASSR/SpringBoot/CourseController.java
@@ -20,7 +20,7 @@ public class CourseController {
 				new Course(6,"Spring Security","Sajal Rastogi")
 				);
 	}
-	@RequestMapping("/name")
+	@RequestMapping("/Brand")
 	public String name() {
 		return "Sajal Rastogi";
 	}
